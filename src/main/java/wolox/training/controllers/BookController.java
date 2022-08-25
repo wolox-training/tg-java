722b969a081fc8972e16cb0c9bdd8def0c43fdd5
tomguillot <tomiguillot@gmail.com>
2022-08-25 15:08:34 -0300

    Updated the controller file
    The view now displays the greeting message

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class BookController {
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
