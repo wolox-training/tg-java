4f6940cd571aaea6d95adbee4359afa881e32775
tomguillot <tomiguillot@gmail.com>
2022-08-26 14:13:32 -0300

    Added BookRepository.java and updated TrainingApplication.java

--- src/main/java/wolox/training/TrainingApplication.java
+++ src/main/java/wolox/training/TrainingApplication.java
@@ -2,7 +2,9 @@ package wolox.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "wolox.training.repositories.BookRepository")
 @SpringBootApplication
 public class TrainingApplication {
 
---
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -0,0 +1,8 @@
+package wolox.training.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import wolox.training.models.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Book findByAuthor(String author);
+}
