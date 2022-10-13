22baaf469a773a8ad7496939a9f1a4fced0b759b
tomguillot <tomiguillot@gmail.com>
2022-10-13 10:37:55 -0300

    Added custom query for getAll method
    Updated url names for creation and retrieving methods on BookController and UserController

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -12,8 +12,15 @@ import wolox.training.models.Book;
 public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findByAuthor(String author);
 
-    @Query("SELECT b FROM Book b WHERE b.publisher = :publisher OR b.genre = :genre"
-            + " OR b.publicationYear = :publicationYear")
+    @Query("select b FROM Book b where b.publisher = :publisher OR b.genre = :genre"
+            + " OR b.publicationYear = :year")
     List<Optional<Book>> findByPublisherOrGenreOrPublicationYear(@Param("publisher")String publisher,
-            @Param("genre")String genre, @Param("publicationYear")String year);
+            @Param("genre")String genre, @Param("year")String year);
+
+    @Query("select b from Book b where b.id = :id OR b.genre = :genre OR b.author = :author"
+            + " OR b.image = :image OR b.title = :title OR b.subtitle = :subtitle"
+            + " OR b.publicationYear = :year OR b.pages = :pages OR b.isbn = :isbn")
+    List<Book> findAll(@Param("id")long id, @Param("genre") String genre, @Param("author") String author,
+            @Param("image")String img, @Param("title")String title, @Param("subtitle") String subtitle,
+            @Param("year") String year, @Param("pages") int pages, @Param("isbn") String isbn);
 }
