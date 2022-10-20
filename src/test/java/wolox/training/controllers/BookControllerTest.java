f0ce990507980bcdb09f2b892c00447b26344ee7
tomguillot <tomiguillot@gmail.com>
2022-10-20 17:36:35 -0300

    Added WireMock to BookControllerTest.java
    Added __files directory inside the test package. Added bookBody.json file to __files directory

--- src/test/java/wolox/training/controllers/BookControllerTest.java
+++ src/test/java/wolox/training/controllers/BookControllerTest.java
@@ -1,56 +1,83 @@
 package wolox.training.controllers;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.time.LocalDate;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpResponse;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import wolox.training.models.Book;
-import wolox.training.models.User;
 import wolox.training.repositories.BookRepository;
 import wolox.training.repositories.UserRepository;
+import wolox.training.services.OpenLibraryService;
 
-@WebMvcTest(BookController.class)
 
+@WireMockTest(httpPort = 8081)
+@AutoConfigureMockMvc
+@SpringBootTest
 class BookControllerTest {
+
+    private static WireMockServer wireMockServer;
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private OpenLibraryService openLibraryService;
 
     @MockBean
     private BookRepository mockBookRepository;
     @MockBean
     private UserRepository mockUserRepository;
 
-    @Test
-    void givenBook_whenGetAllBooks_thenReturnJsonArray() throws Exception{
-        Book testBook = new Book("Science Fiction","George Orwell","cover_image.jpg"
-                ,"1984","-","Secker & Warburg","1949",328,"470015866");
-        List<Book> allBooks = Arrays.asList(testBook);
+    @BeforeAll
+    static void init(){
+        wireMockServer = new WireMockServer();
+        wireMockServer.start();
+        configureFor("localhost", 8081);
+    }
 
-        Mockito.when(mockBookRepository.findAll()).thenReturn(allBooks);
-        mvc.perform(MockMvcRequestBuilders.get("/api/books").contentType(MediaType.APPLICATION_JSON))
+    @Test
+    @WithMockUser
+    void givenBook_whenGetAllBooks_thenReturnJsonArray() throws Exception{
+        Mockito.when(mockBookRepository.findAll(1,null,null,null,null,
+                null,null,0,null, PageRequest.of(0,1))); //.thenReturn(allBooks);
+        mvc.perform(MockMvcRequestBuilders.get("/api/books/all").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(1)));
     }
 
     @Test
+    @WithMockUser
     void whenFindABookByIdThatExists_thenReturnJson() throws Exception{
         Book testBook = new Book("Science Fiction","George Orwell","cover_image.jpg"
                 ,"1984","-","Secker & Warburg","1949",328,"470015866");
@@ -66,6 +93,7 @@ class BookControllerTest {
     }
 
     @Test
+    @WithMockUser
     void whenFindABookByIdThatDoesNotExist_thenThrowException(){
         Book testBook = new Book("Science Fiction","George Orwell","cover_image.jpg"
                 ,"1984","-","Secker & Warburg","1949",328,"470015866");
@@ -77,6 +105,7 @@ class BookControllerTest {
     }
 
     @Test
+    @WithMockUser
     void whenFindByAuthorThatExists_thenReturnJson() throws Exception{
         Book testBook = new Book("Science Fiction","George Orwell","cover_image.jpg"
                 ,"1984","-","Secker & Warburg","1949",328,"470015866");
@@ -92,6 +121,7 @@ class BookControllerTest {
     }
 
     @Test
+    @WithMockUser
     void whenFindByAuthorThatDoesNotExist_thenThrowException(){
         Book testBook = new Book("Science Fiction","George Orwell","cover_image.jpg"
                 ,"1984","-","Secker & Warburg","1949",328,"470015866");
@@ -103,9 +133,14 @@ class BookControllerTest {
 
     @Test
     @WithMockUser
-    void whenSearchByIsbnAndDoesNotExist_thenThrowException() throws Exception {
-        Mockito.when(mockBookRepository.findByIsbn("123")).thenReturn(null);
-        mvc.perform(MockMvcRequestBuilders.get("/api/books/isbn/123")
-                .contentType(MediaType.APPLICATION_JSON));
+    void whenSearchByIsbnAndDoesNotExist_thenFindItOnOpenLibrary() throws Exception {
+        stubFor(WireMock.get("/api/books/isbn/0451521234").willReturn(aResponse()
+                .withStatus(200)
+                .withHeader("Content-Type", "application/json")
+                .withBodyFile("bookBody.json")));
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet request = new HttpGet("http://localhost:8081/api/books/isbn/0451521234");
+        HttpResponse response = httpClient.execute(request);
     }
+
 }
